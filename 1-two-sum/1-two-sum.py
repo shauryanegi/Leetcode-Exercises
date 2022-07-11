b8ef9fcf3cac17df1ad56ba3f4701ebe6ab84d7c
Shaurya Negi <shauryanegi17@gmail.com>
2022-07-11 22:45:14 +0530

    Time: 93 ms (66.52%), Space: 15.2 MB (50.37%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        # Time Complexity: O(n)
+        # Memory Complexity: O(n) -> adding a hashmap
+        
+        prevMap = {} # val -> index
+        
+        for index, number in enumerate(nums):
+            diff = target - number
+            
+            if diff in prevMap:
+                return [prevMap[diff], index]
+            
+            prevMap[number] = index
+            
+        return
+            
+            
+            
+        
